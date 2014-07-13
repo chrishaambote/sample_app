72772887136d43031b8d1862b4616e766c774471
Chris Haambote <chrishaambote@gmail.com>
2014-07-13 10:40:35 +0800

    implemented Devise for user

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 gem 'bootstrap-sass'
-
+gem 'devise', '~> 3.1.0.rc2'
 group :development, :test do
      gem 'sqlite3'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # required for heroku
+  # not to set this to actual host name
+config.action_mailer.default_url_options = { :host => 'drinkupzam.com'}
 end
